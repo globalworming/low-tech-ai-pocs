735fce6dac30c23c6496da2c117911f4cbc75daa
globalworming <andreas.worm@headissue.com>
2025-07-18 15:06:39 +0200

    revert

--- twitch_chat_bot/bot.py
+++ twitch_chat_bot/bot.py
@@ -261,28 +261,24 @@ class MinimalTwitchBot(commands.AutoBot):
             raise e
 
     async def periodic_summary_post(self):
+        """Post messages to cloud function"""
         while True:
+            
+            await asyncio.sleep(8)
+            
             if not self.p1_messages and not self.p2_messages:
                 continue
-            p1_first = choice([True, False])
-            if game_state.p1.health > game_state.p2.health:
-                p1_first = False
-            elif game_state.p2.health > game_state.p1.health:
-                p1_first = True
 
-            if p1_first:
-                if self.p1_messages:
-                    await self._process_player_summary(game_state.p1, self.p1_messages)
-                    await asyncio.sleep(10)            
-                if self.p2_messages:
-                    await self._process_player_summary(game_state.p2, self.p2_messages)
-            else:
-                if self.p2_messages:
-                    await self._process_player_summary(game_state.p2, self.p2_messages)
-                    await asyncio.sleep(10)
-                if self.p1_messages:
-                    await self._process_player_summary(game_state.p1, self.p1_messages)
-            await asyncio.sleep(10)
+            # Process both players
+            if self.p1_messages:
+                await self._process_player_summary(game_state.p1, self.p1_messages)
+            
+            await asyncio.sleep(9)
+             
+            if self.p2_messages:
+                await self._process_player_summary(game_state.p2, self.p2_messages)
+
+
             
     async def _update_server_state(self):
         """Update server state via REST call"""
